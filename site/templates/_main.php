f7a2b59e773041372edcbadd1c8f24a7bf51fbec
ff <nandofuentes01@gmail.com>
2023-06-28 09:14:42 -0600

    Home first sections

--- site/templates/_main.php
+++ site/templates/_main.php
@@ -5,9 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/uikit.min.css" />
-    <script src="js/uikit.min.js"></script>
-    <script src="js/uikit-icons.min.js"></script>
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.19/dist/css/uikit.min.css" />
+
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.19/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.19/dist/js/uikit-icons.min.js"></script>
     <title>Document</title>
 </head>
 <body>
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,139 +1,189 @@
-<?php 
+<?php namespace ProcessWire; ?>
+<?php
 $configuracion = $pages->get("name=configuracion");
 ?>
-<div class="uk-margin-top uk-grid">
-	<div class="uk-margin-top uk-width-medium-2-3">
-		<div id="homepage-slider">
-			<?php foreach($configuracion->slider as $item):  ?>
-				<div class="uk-cover-background main-slider-item"
-					style="background-image:url('<?php 
-												 if($item->slider_image->count()){
-													 echo $item->slider_image->first()->media->width(650)->url;
-												 }else{
-													 echo ""; 
-												 }
-												 ?>');">
-					
-					<div class="slider-title">
-						<h1><a href="<?php echo $item->slider_page->url ?>"><?php echo $item->slider_page->title ?></a></h1>
+<region id="content">
+	<div class="uk-container uk-margin">
+		<div class="" uk-grid>
+			<div class="uk-width-1-4@m">
+				<img src="<?php echo $configuracion->logo->width(250)->url ?>">
+
+			</div>
+
+			<div class="uk-width-3-4@m">
+				<h1 class="uk-text-center">Aqui va la publicidad</h1>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="uk-container" >
+			<div>
+				<nav class="uk-navbar-container" uk-navbar>
+					<div class="uk-navbar-center">
+						<ul class="uk-navbar-nav">
+							<?php $menu = $configuracion->menu ?>
+							<?php foreach($menu as $menuItem): ?>
+
+								<?php if($menuItem->menu_render_submenu): ?>
+									<li>
+										<a  href="#">
+											<?php
+											/** @var RepeaterPage $menuItem  */
+											if($menuItem->title){
+												echo $menuItem->title;
+											}else{
+												// TODO Fix page field name
+												echo $menuItem->menu_item->title;
+											}
+											// How to do with $menuItem->if()
+											// Can also be done with a hook, $wire->addNewMethod('Page::getMenuTitle', function(){)
+											// echo $page->getMenuTitle();
+											?>
+										</a>
+										<div class="uk-navbar-dropdown">
+											<ul class="uk-nav uk-navbar-dropdown-nav">
+												<?php foreach($menuItem->menu_item->children as $submenuItem): ?>
+													<li>
+														<a href="<?=$submenuItem->url?>">
+															<?=$submenuItem->title?>
+														</a>
+													</li>
+												<?php endforeach ?>
+											</ul>
+										</div>
+									</li>
+								<?php else: ?>
+
+									<li class="">
+										<a href="">
+											<?php
+											if($menuItem->title){
+												echo $menuItem->title;
+											}else{
+												// TODO Fix page field name
+												echo $menuItem->menu_item->title;
+												bd($menuItem->menu_item);
+											}
+											?>
+										</a>
+									</li>
+								<?php endif ?>
+							<?php endforeach ?>
+						</ul>
+					</div>
+				</nav>
+			</div>
+	</div>
+
+
+	<div class="uk-container uk-margin-top">
+			<div id="homepage-slider" uk-grid>
+				<div class="uk-width-3-5@m">
+					<?php foreach($configuracion->slider as $item):  ?>
+					<div class="uk-inline">
+						<img src="<?php echo $item->slider_image->first()->media->width(700)->url; ?>">
+							<div class="uk-overlay uk-overlay-primary uk-position-bottom">
+								<p><a href="<?php echo $item->slider_page->url ?>"><?php echo $item->slider_page->title ?></a></p>
+							</div>
+					</div>
+					<?php endforeach; ?>
+				</div>
+
+				<div class="uk-width-2-5@m">
+					<h3 class="uk-text-center"><span class="uk-text-underline">Versión impresa</span></h3>
+					<?php $issue =  $pages->find("template=periodico")->sort('-published')->first(); ?>
+					<a href="<?php echo $issue->url ?>">
+						<?php if($issue->image): ?>
+							<img class="periodico-thumb" style="margin:auto;display:block;"
+								 class="uk-width-medium-6-10"
+								 src="<?php echo $issue->image->height(230)->url ?>">
+						<?php else: ?>
+							<img class="periodico-thumb" style="margin:auto;display:block;"
+								 class="uk-width-medium-6-10"
+								 src="<?php echo $issue->periodico_pdf->toImage()->height(230)->url ?>">
+						<?php endif ?>
+
+					</a>
+					<p class="uk-margin-top uk-text-center" ><?php echo $issue->title ?></p>
+				</div>
+			</div>
+		</div>
+
+
+	<div class="uk-container uk-margin-top ">
+		<h3 class="light-header ">Otras notas y artículos</h3>
+		<hr>
+	</div>
+
+	<div class="uk-container uk-margin-top">
+		<div class="uk-child-width-1-2@m uk-grid-match "uk-grid>
+
+			<?php foreach ($configuracion->notas_resaltadas as $article): ?>
+				<div>
+					<div class="uk-card uk-card-default">
+						<div class="uk-card-media-top uk-cover-container uk-height-medium">
+							<img src="<?php echo $article->article_images->first()->media->size(300, 150)->url; ?>"
+								 alt="" uk-cover>
+						</div>
+
+						<div class="uk-card-body">
+							<a href="<?php echo $article->url ?>"><h3
+									class="uk-card-title"><?php echo $article->title ?></h3></a>
+
+							<p>
+								<?= $sanitizer->truncate($article->body, array(
+									'type' => 'punctuation',
+									'maxLength' => 150,
+									'visible' => true,
+									'more' => '...'
+								)); ?>
+							</p>
+							<a class="read-more uk-flex uk-flex-right" href="<?php echo $article->url ?>">Leer más &#10161;</a>
+						</div>
 					</div>
 				</div>
 			<?php endforeach; ?>
-		</div> 
-	</div>
-	
-	<div id="ladosistv-column" class="uk-margin-top uk-width-medium-1-3">
-		<!-- <div id="ladosistv" class="video-container">
-			 <?php $video =  $pages->find("template=ladosis-tv-video")->sort('-published')->first(); ?>
-			 <h3 class="light-header underlined-title">La Dosis TV: <?php echo $video->tv_video_title ?></h3>
-			 <?php echo $video->tv_embed_code ?>
-			 <div class="video-description">
-			 <?php
-			 if($video->tv_video_descripcion_corta == ''){
-			 echo $video->tv_video_descripcion;
-			 } else{
-			 echo $video->tv_video_descripcion_corta;
-			 }
-			 ?>
-			 </div> 
-			 </div> -->
-
-		
-
-	
-		
-		<h3 class="light-header underlined-title">Versión impresa</h3>
-		<?php $issue =  $pages->find("template=periodico")->sort('-published')->first(); ?>
-		<a href="<?php echo $issue->url ?>">
-            <?php if($issue->image): ?>
-                <img class="periodico-thumb" style="margin:auto;display:block;"
-				    class="uk-width-medium-6-10"
-				    src="<?php echo $issue->image->height(230)->url ?>">
-            <?php else: ?>
-                <img class="periodico-thumb" style="margin:auto;display:block;"
-				    class="uk-width-medium-6-10"
-				    src="<?php echo $issue->periodico_pdf->toImage()->height(230)->url ?>">
-            <?php endif ?>
-			
-		</a>
-		<p class="uk-margin-top" ><?php echo $issue->title ?></p>
-		
-
-		
-		
+		</div>
 	</div>
 
-
-	<div class="uk-width-1-1 uk-margin-top ">
-		<h3 class="light-header underlined-title">Otras notas y artículos</h3>
+	<div class="uk-container uk-margin-large-top ">
+		<h3 class="light-header ">Notas recientes:</h3>
+		<hr>
 	</div>
-	<?php foreach($configuracion->notas_resaltadas as $article): ?>
-		<div class="article-list 
-					uk-margin-top
-					uk-width-small-5-10 
-					uk-width-medium-5-10">
-			<a  href="<?php echo $article->url ?>">   <h3 class=""><?php echo $article->title ?></h3></a>
-			<?php if(count($article->article_images)):?>
-				<a class="article-image" href="<?php echo $article->url?>">
-                    <?php if($article->article_images->count): ?>
-					<img class="img-responsive"
-						src="
-								<?php
-								//$log->save("slider", $article->article_images->count());
-								if($article->article_images->count()){
-									echo $article->article_images->first()->media->size(500, 200,['cropping' => 'center'])->url;
-								}else{
-									echo "";
-								}
-								
-								?>
-								">
-                    <?php endif ?>
-				</a>
-			<?php endif; ?>
-			
-			<p>
-				<?php
-				//if(count($article->article_images) == 0)
-				echo $article->wordLimiter("body", 100);
-				?>
-				
-			</p>
-			<a class="read-more" href="<?php echo $article->url ?>">Leer más &#10161;</a>
-		</div> 
-	<?php endforeach; ?>
 
-	<?php/* wireIncludeFile("inc/news-item.php", array("article" => $article)); */?>
-	
-	
-	<div class="uk-width-1-1 uk-margin-top ">
-		<h3 class="light-header underlined-title">Notas recientes:</h3>
+	<div class="uk-container uk-margin-top">
+		<div class="uk-child-width-1-5@m uk-grid-match "uk-grid>
+
+			<?php foreach ($configuracion->featured as $article): ?>
+				<div>
+					<div class="uk-card uk-card-default">
+						<div class="uk-card-media-top uk-cover-container uk-height-medium">
+							<img src="<?php echo $article->article_images->first()->media->size(300, 150)->url; ?>"
+								 alt="" uk-cover>
+						</div>
+
+						<div class="uk-card-body">
+							<a href="<?php echo $article->url ?>"><h3
+									class="uk-card-title"><?php echo $article->title ?></h3></a>
+
+							<p>
+								<?= $sanitizer->truncate($article->body, array(
+									'type' => 'punctuation',
+									'maxLength' => 150,
+									'visible' => true,
+									'more' => '...'
+								)); ?>
+							</p>
+							<a class="read-more uk-flex uk-flex-right" href="<?php echo $article->url ?>">Leer más &#10161;</a>
+						</div>
+					</div>
+				</div>
+			<?php endforeach; ?>
+		</div>
 	</div>
-	
-	<?php foreach($pages->find("template=articulo|carton, sort=-published, limit=5") as $article): ?>
-		<?php wireIncludeFile("inc/article-item.php", array("article" => $article)); ?>
-	<?php endforeach ?>
-	
 
-    <?php $mostPopular = $configuracion->featured; ?>
-    
-    <?php if($mostPopular->count): ?>
-	    <div class="uk-width-1-1 uk-margin-top ">
-		    <h3 class="light-header underlined-title">Lo más popular</h3>
-	    </div> 
-	    
-	    
-	    <?php
-        foreach($mostPopular as $i => $article){
-		    if ($i == 10) break;
-		    echo wireRenderFile("inc/article-item.php", array("article" => $article));
-		    
-	    }
-	    
-	    ?>
-    <?php endif ?>
-	
-</div>
+</region>
 
 
 
